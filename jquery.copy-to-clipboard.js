45d5158f9fc60120492a79f72f21e88a7946c7a7
Milan <mmkyncl@gmail.com>
2016-04-17 02:59:49 +0200

    Create jquery.copy-to-clipboard.js

---
+++ jquery.copy-to-clipboard.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016 Milan Kyncl
+ * Licensed under the MIT license.
+ *
+ * jquery.copy-to-clipboard plugin
+ * https://github.com/mmkyncl/jquery-copy-to-clipboard
+ *
+ */
+
+$.fn.CopyToClipboard = function() {
+    var textToCopy = false;
+    if(this.is('select') || this.is('textarea') || this.is('input')){
+        textToCopy = this.val();
+    }else {
+        textToCopy = this.text();
+    }
+    CopyToClipboard(textToCopy);
+};
+
+function CopyToClipboard( val ){
+    var hiddenClipboard = $('#_hiddenClipboard_');
+    if(!hiddenClipboard.length){
+        $('body').append('<textarea style="position:absolute;top: -9999px;" id="_hiddenClipboard_"></textarea>');
+        hiddenClipboard = $('#_hiddenClipboard_');
+    }
+    hiddenClipboard.html(val);
+    hiddenClipboard.select();
+    document.execCommand('copy');
+    document.getSelection().removeAllRanges();
+}
+
+$(function(){
+    $('[data-clipboard-target]').each(function(){
+        $(this).click(function() {
+            $($(this).data('clipboard-target')).CopyToClipboard();
+        });
+    });
+    $('[data-clipboard-text]').each(function(){
+        $(this).click(function(){
+            CopyToClipboard($(this).data('clipboard-text'));
+        });
+    });
+});
